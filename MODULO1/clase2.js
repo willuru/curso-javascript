6f7c096743245e713ffc37cb4df316258822a7a6
willuru <104177025+willuru@users.noreply.github.com>
2022-05-02 22:59:10 -0500

    carga de las 3 primeras clases

---
+++ MODULO1/clase2.js
@@ -0,0 +1,60 @@
+//esta es la variable de la edad de willmer 
+var edad_de_wilmer = 30
+// para poder imprimir el valor de mi variable 
+console .log (edad_de_wilmer)
+
+//tipos de datos 
+//Nuerico  (integer, number, int)
+var numero = 10
+var numero2 = 1000
+var numero3 = 3.1416
+//Texto (string, texto)
+var Texto1 = 'Hola como estas'
+var Texto2 = 'Hola mundo'
+var Texto = '10'
+//booleanos (bool, boolean)
+var boolean = true // verdadero 
+var boolean2 = false // falso
+//Arreglos, listas (array, list)
+var lista2 = [willmer, Momo, Diego]
+var lista3 = [true, true, false]
+var lista4 = [true, 100 'Hola']
+var listaVacia = []
+var listaDeVariables = [numero, Texto1, boolean2]
+var elemento4 = lista1[3] // elemento4 = 4
+
+//constante 
+const pi = 3.1416
+pi = "esta es constante pi"
+alert(pi)
+
+
+//modificadores
+// para numbers
+var numero4 = 100
+var copiaNumero4 = numero4 //copia de una variable 
+//numero4 = numero4 - 10
+numero4 = numero4 + pi// numero4 +3.1416 = 103.1416
+numero4 = numero4 *200 + 2 + 20 / 34
+numero4 = numero4 /2
+
+//texto
+var hola = 'hola '
+var como = ' como '
+var estas = 'estas'
+var saludo = hola+como+estas
+alert(saludo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ MODULO1/clase3.js
@@ -0,0 +1,5 @@
+//tipoa de datos nulos e indefinidos 
+const constanteNula = null
+var constanteIndefinida = undefined
+//Terminar modificaciones de lista
+var lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
