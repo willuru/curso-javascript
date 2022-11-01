ba9e769a45f971f988593fc08f54390387460e10
willuru <104177025+willuru@users.noreply.github.com>
2022-11-01 16:53:25 -0500

    Clases 1-2

---
+++ MODULO1/Clase2.js
@@ -0,0 +1,52 @@
+// Esta es la variable de edad de willmer
+var edadWillmer = 31
+//Para poder imprimir el valor de una variable nesecitamos hacer console..log
+console.log(edadWillmer)
+
+// Tipos de datos
+// Numericos (integer, number, int)
+var numero = 10
+var numero2 = 1000
+var numero3 = 3.1416
+// Texto (string, text)
+var texto1 = 'hola como estas'
+var texto2 = 'hola mundo'
+var texto3 = '10'
+// booleanos  (bool, boolean)
+var boolean = true //verdadero
+var boolean2 = false // falso
+// Arreglos, listas (array, list)
+var lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var lista2 = ['Willmer, Momo, Diego']
+var lista3 = [true, true, false]
+var lista4 = [true, 100, 'Hola']
+var listaVacia = []
+var listaVariables = [numero, texto1, boolean2]
+elemento4 = lista1[3] // elemento = 4
+
+//Constantes
+const pi = 3.1416
+pi = "Esta es mi constante pi"
+alert(pi)
+
+//Modificadores
+//Para numbers
+var numero4 = 100
+var copiaNumero4 = numero4// Copia de una variable
+//numero4 = numero4 - 10
+numero4 = numero4 + pi // nummero4 = 100 + 3.1416 = 103.1416
+numero4 = numero4 *200 + 2 / 34
+numero4 = numero4 / 2
+
+//Texto
+var hola = "Hola"
+var como = "como"
+var estas = "estas"
+var saludo = `${hola} ${como} ${estas}` 
+alert(saludo)
+
+//Booleanos
+
+//Listas
+
+//Condiiones
---
+++ MODULO1/Clase3.js
@@ -0,0 +1,5 @@
+// Tipos de dotos nulos e indifinidos 
+const constanteNula = null
+var constanteIsifinida = undefined
+constanteIsifinida = 0
+// Terminar modificadores de lista
